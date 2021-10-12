80191a50367b7a5a9fdab0586eba474ddac20c73
abhaypapu <89897824+abhaypapu@users.noreply.github.com>
2021-10-12 15:25:11 +0530

    Add files via upload

---
+++ Instrument.js
@@ -0,0 +1,15 @@
+// JavaScript code
+function search_Instrument() {
+    let input = document.getElementById('searchbar').value
+    input=input.toLowerCase();
+    let x = document.getElementsByClassName('Instrument');
+      
+    for (i = 0; i < x.length; i++) { 
+        if (!x[i].innerHTML.toLowerCase().includes(input)) {
+            x[i].style.display="none";
+        }
+        else {
+            x[i].style.display="list-item";                 
+        }
+    }
+}
